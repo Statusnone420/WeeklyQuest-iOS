8eb54b94fee3ced98825ae30a183981ebeeb548c
Statusnone420 <anthonyga88@icloud.com>
2025-11-30 19:43:00 -0500

    Initiate SwiftUI Push

---
+++ QuestChatNative/QuestChatNative/QuestChatNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestChatNativeApp.swift
+//  QuestChatNative
+//
+//  Created by Anthony Gagliardo on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct QuestChatNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
